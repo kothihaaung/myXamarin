74a56845716cefc7c523bf1a4529015fed741dd6
Appvantage Myanmar <thiha@appvantage.asia>
2017-03-07 12:49:04 +0630

    new datasource for list view grouping

--- HelloForms/HelloForms/CollapsableView.xaml.cs
+++ HelloForms/HelloForms/CollapsableView.xaml.cs
@@ -6,6 +6,18 @@ using Xamarin.Forms;
 
 namespace HelloForms
 {
+	class CarGroup
+	{
+		public string name { get; set; }
+		public ObservableCollection<Car> carList;
+	}
+
+	class Car
+	{
+		public string brand { get; set; }
+		public string year { get; set; }
+	}
+
 	public partial class CollapsableView : ContentPage
 	{
 		public Command buttonClickCommand { get; set; }
@@ -34,24 +46,39 @@ namespace HelloForms
 
 			absLayout.Children.Add(centerLabel);
 
-			var dataSource = new ObservableCollection<String> { 
-				"ByObservableCol",
-			  	"monodroid",
-			  	"monotouch",
-			  	"monorail",
-			  	"monodevelop",
-			  	"monotone",
-			  	"monopoly",
-			  	"monomodal",
-			  	"mononucleosis"
-			};
+			var toyotaSUV = new Car { brand = "TOYOTA", year = "2002" };
+			var nissanSUV = new Car { brand = "NISSAN", year = "2013" };
 
-			myListView.ItemsSource = dataSource;
+			var toyotaTruck = new Car { brand = "TOYOTA", year = "2008" };
+			var nissanTruck = new Car { brand = "NISSAN", year = "2015" };
+
+			var toyotaSaloon = new Car { brand = "TOYOTA", year = "2007" };
+			var nissanSaloon = new Car { brand = "NISSAN", year = "2016" };
+
+			var carGroup = new ObservableCollection<CarGroup> ();
+
+			var carSuvList = new ObservableCollection<Car>();
+			carSuvList.Add(toyotaSUV);
+			carSuvList.Add(nissanSUV);
+
+			var carTruckList = new ObservableCollection<Car>();
+			carTruckList.Add(toyotaTruck);
+			carTruckList.Add(nissanTruck);
+
+			var carSaloonList = new ObservableCollection<Car>();
+			carSaloonList.Add(toyotaSaloon);
+			carSaloonList.Add(nissanSaloon);
+
+			carGroup.Add(new CarGroup { name="SUV", carList=carSuvList });
+			carGroup.Add(new CarGroup { name = "Truck", carList = carTruckList });
+			carGroup.Add(new CarGroup { name = "Saloon", carList = carSaloonList });
+
+			myListView.ItemsSource = carGroup;
 
 			// button click
-			buttonClickCommand = new Command<String> ((string butTitle) => { 
+			buttonClickCommand = new Command<CarGroup> ((removingCarGroup) => { 
 			
-				dataSource.Remove((String)butTitle);
+				carGroup.Remove(removingCarGroup);
 			});
 		}
 	}
